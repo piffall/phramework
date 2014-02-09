669d15433a86fcd16e12633badd7cadeacfb1100
Cristòfol Torrens <piffall@gmail.com>
2014-02-09 21:11:36 +0100

    PHKCurl started

--- classes/PHKCurl.php
+++ classes/PHKCurl.php
@@ -10,5 +10,40 @@
 
 class PHKCurl
 {
-    
+    protected $Curl = null;
+
+    protected $URL = '';
+    protected $Port = 80;
+    protected $UserAgent = '';
+    protected $Timeout = 5;
+    protected $DataFields = array();    
+
+	/**
+	 * Constructor
+	 * Initializes cURL handler
+	 */
+    public function __construct()
+    {
+        $this->Curl = curl_init();
+    }
+
+	/**
+	 * Close
+	 * Closes cURL handler
+	 */
+    public function close()
+    {
+        curl_close($this->Curl);
+    }
+
+	/**
+	 * Destructor
+	 */
+    public function __destruct()
+    {
+        $this->close();
+    }
+
 }
+
+?>

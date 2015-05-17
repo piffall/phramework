5ffc6f96dcbd368c2e1eab64767db8b3f9355fbf
Cristòfol Torrens <piffall@gmail.com>
2015-05-17 12:39:07 +0200

    License in files head

--- src/Curl.php
+++ src/Curl.php
@@ -1,12 +1,20 @@
 <?php
-##########################################################################
-#                                                                        #
-# Cristòfol Torrens Morell <piffall@gmail.com>                           #
-#                                                                        #
-# Released under the GNU General Public License WITHOUT ANY WARRANTY.    #
-# See LICENSE file for more information                                  #
-#                                                                        #
-##########################################################################
+/**
+This file is part of phramework.
+
+Foobar is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+Foobar is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with phramework.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
 namespace PHK;
 class Curl
